2330d720aec77d56faeca6b64a408dbed7a9c917
Robert <142694357+GIGI-CodeAce@users.noreply.github.com>
2023-11-23 18:02:59 +0200

    Add files via upload

---
+++ menuScript.js
@@ -0,0 +1,37 @@
+const nrSCdown = document.getElementById("nrSCdown")
+const nrSCup = document.getElementById("nrSCup")
+
+let Array1 = []
+let Array2 = []
+let A1length = 0
+let numbersMovement = 0
+let boolean = false
+
+const startBTN = document.getElementById("start-btn")
+
+startBTN.addEventListener("click", function(){
+    window.location = "./CGconfig";
+});
+
+function idkNameYet() {
+    A1length++
+    if (A1length == 50) {
+        boolean = true
+    }
+    if (boolean) {
+        numbersMovement += 10
+    }
+    if (boolean) {
+        nrSCup.style.transform = `translateX(${-numbersMovement}px)`
+        nrSCdown.style.transform = `translateX(${-numbersMovement}px)`
+    }
+    let randNum = Math.floor(Math.random() * 9) 
+    let randNum2 = Math.floor(Math.random() * 9)
+    nrSCup.textContent = Array1.join('') + randNum
+    nrSCdown.textContent = Array2.join('') + randNum2
+    Array1.push(randNum)
+    Array2.push(randNum2)
+}
+
+setInterval(idkNameYet, 15)
+    
