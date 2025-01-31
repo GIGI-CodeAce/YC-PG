674cc1cd89493fea98711c68632c030516cb1e3d
Dobre Robert <dobrerobert441@gmail.com>
2025-01-31 15:29:51 +0200

    sorted files into folders(testing)

---
+++ Editor/genScript.js
@@ -0,0 +1,151 @@
+import { Characterss, Numberss, Specials, charQuan, genQuan, tempSelected, joinType, Uppers } from './configScript.js';
+
+let GIGI = "GIGI-CodeAce";
+let ty = "Thanks-ForUsingYC&PG";
+let ArrayUI = [];
+const nrScUI = document.getElementById("GIGI");
+const codeBoard = document.querySelector(".code-board");
+let int = 0;
+let charactersBN = false;
+let numbersBN = true;
+let specialsBN = false;
+let capsBN = false
+let count = 0;
+
+switch (Numberss) {
+  case 1:
+    numbersBN = true;
+    break;
+  default:
+    numbersBN = false
+    break;
+}
+switch (Characterss) {
+  case 1:
+    charactersBN = true;
+    break;
+    default:
+    charactersBN = false
+    break;
+}
+switch (Specials) {
+  case 1:
+    specialsBN = true;
+    break;
+    default:
+    specialsBN = false
+    break;
+}
+switch (Uppers) {
+  case 1:
+    capsBN = true
+    break;
+    default:
+    capsBN = false
+    break;
+}
+
+const alphabetArray = [
+  'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+  'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+  'u', 'v', 'w', 'x', 'y', 'z'
+];
+
+let alphabet
+
+if (capsBN) {
+  alphabet = alphabetArray.map(letter => letter.toUpperCase());
+} else {
+  alphabet = alphabetArray.map(letter => letter.toLowerCase());
+}
+
+const specialCharacters = [
+  '!', '@', '#', '$', '%', '^', '&', '*', '(', ')',
+  '-', '_', '+', '=', '[', ']', '{', '}', '|', '\\',
+  ';', ':', '"', '\'', ',', '.', '/', '?'
+];
+
+function getRandomItemFromArray(array) {
+  const randomIndex = Math.floor(Math.random() * array.length);
+  return array[randomIndex];
+}
+
+
+document.body.addEventListener("click", event => {
+  if (event.target.classList.contains(`copy-btn`)) {
+    event.target.textContent = "soon.";
+    setTimeout(() => {
+      event.target.textContent = "copy";
+    }, 696);
+  }
+});
+
+
+function render() {
+  let availableNums = [0, 1, 2];
+  function cut(number){
+    const indexToRemove = availableNums.indexOf(number);
+    if (indexToRemove !== -1) {
+      availableNums.splice(indexToRemove, 1);
+    }
+  }
+  if (!specialsBN) {
+    cut(2)
+  }if (!charactersBN) {
+    cut(1)
+  }if (!numbersBN) {
+    cut(0)
+  }
+
+  if (count < genQuan) {
+    count++;
+    let liContent = "";
+    let temp = 0;
+
+    for (let i = 0; i < charQuan; i++) {
+      let randIndex = Math.floor(Math.random() * availableNums.length);
+      let randomNum = availableNums[randIndex];
+
+if (randomNum === 0) {
+        liContent += Math.floor(Math.random() * 10);
+      } else if (randomNum === 1) {
+        liContent += getRandomItemFromArray(alphabet);
+      } else {
+        liContent += getRandomItemFromArray(specialCharacters);
+      }
+
+      if (tempSelected == 1) {
+        temp++;
+        if (temp % 3 === 0 && temp > 0) {
+          liContent += joinType
+        }
+      }
+
+      if (tempSelected == 2) {
+        temp++;
+          if (temp % 4 === 0 && temp > 0) {
+            liContent += joinType
+      
+         }}
+
+    }
+
+    let listItems = `<button class = "copy-btn" >copy</button><li id="li#${count}">${liContent}</li>`;
+      codeBoard.innerHTML += listItems;
+  }
+}
+
+setInterval(render, 25);
+
+function background() {
+  int++;
+  ArrayUI.push(GIGI);
+  ArrayUI.push(ty);
+  nrScUI.textContent = ArrayUI.join(" // ");
+
+  if (int < 250) {
+    setTimeout(background, 2);
+  }
+}
+
+background();
---
+++ Generation/configScript.js
@@ -0,0 +1,231 @@
+
+export let Characterss = parseInt(localStorage.getItem("characters")) || 2;
+export let Numberss = parseInt(localStorage.getItem("numbers")) || 1;
+export let Specials = parseInt(localStorage.getItem("specials")) || 2;
+export let Uppers = parseInt(localStorage.getItem("upper")) || 2;
+
+export let genQuan = parseInt(localStorage.getItem("Characters-quantity")) || 1;
+export let charQuan = parseInt(localStorage.getItem("generates-quantity")) || 1;
+export let tempSelected = parseInt(localStorage.getItem("tempSel")) || 0;
+export let joinType = localStorage.getItem("type") || "-";
+
+ const generateBTN = document.getElementById("generate-btn")
+
+ if (generateBTN){
+    tempSelected = 0
+     localStorage.setItem("type", "-");
+ }
+
+document.addEventListener("DOMContentLoaded", function(){
+    let ArrayUI = [];
+    const nrScUI = document.getElementById("nrSC");
+
+    const templates = document.querySelector(".layer4");
+    const templateTwo = templates.querySelector(".template2");
+    const templateOne = templates.querySelector(".template1");
+    const hiddenEl2 = document.querySelector(".hidden2")
+    const hiddenEl1 = document.querySelector(".hidden1")
+
+    const unSelect = document.getElementById("un-select")
+    const charValue = document.getElementById("Characters-quantity")
+    const genValue = document.getElementById("generates-quantity")
+    
+    const errorEl = document.getElementById("errors")
+    
+    const yesNumbers = document.getElementById("yes-numbers")
+    const noNumbers = document.getElementById("no-numbers")
+
+    const yesCaps = document.getElementById("yes-caps")
+    const noCaps = document.getElementById("no-caps")
+
+    const noSpecials = document.getElementById("no-special")
+    const yesSpecials = document.getElementById("yes-special")
+    
+    const yesLetters = document.getElementById("yes-letters")
+    const noLetters = document.getElementById("no-letters")
+    const joinPattern = document.getElementById("join-pattern")
+    const backBTN = document.getElementById("back-btn")
+
+    backBTN.addEventListener("click", function(){
+        tempSelected = 0
+    })
+
+        noLetters.addEventListener("click", function(){
+            Characterss = 2
+            buttonsStyle()
+        })
+        yesLetters.addEventListener("click", function(){
+            Characterss = 1
+            hiddenEl1.style.backgroundColor = "hsla(240, 100%, 70%, 0.233)";
+            buttonsStyle()
+            setTimeout(() => {
+                hiddenEl1.style.backgroundColor = "hsla(240, 100%, 70%, 0.192)";
+            }, 222);
+        })
+        yesNumbers.addEventListener("click", function(){
+            Numberss = 1
+            buttonsStyle()
+        })
+        noNumbers.addEventListener("click", function(){
+            Numberss = 2
+            buttonsStyle()
+        })
+        yesSpecials.addEventListener("click", function(){
+            Specials = 1
+            buttonsStyle()
+        })
+        noSpecials.addEventListener("click", function(){
+            Specials = 2
+            buttonsStyle()
+        })
+        yesCaps.addEventListener("click", function(){
+            Uppers = 1
+            buttonsStyle()
+        })
+        noCaps.addEventListener("click", function(){
+            Uppers = 2
+            buttonsStyle()
+        })
+    
+    function buttonsColoring1(yesValue, noValue){
+        yesValue.style.borderColor = "black"
+        yesValue.style.color = "black"
+        yesValue.style.borderRadius = "5px"
+        noValue.style.borderColor = "hsla(240, 100%, 70%, 0.712)"
+        noValue.style.color = "hsla(240, 100%, 70%, 0.712)"
+        noValue.style.borderRadius = "10px"
+    
+    }
+    function buttonsColoring2(yesValue, noValue){
+        noValue.style.borderColor = "black"
+        noValue.style.color = "black"
+        noValue.style.borderRadius = "5px"
+        yesValue.style.borderColor = "hsl(240, 100%, 70%)"
+        yesValue.style.color = "hsl(240, 100%, 70%)"
+        yesValue.style.borderRadius = "10px"
+    
+    }
+
+    
+    function buttonsStyle(){
+        switch (Characterss) {
+            case 1:
+                buttonsColoring2(yesLetters, noLetters)
+                hiddenEl1.style.display = "block"
+            break;
+            case 2:
+                buttonsColoring1(yesLetters, noLetters)
+                hiddenEl1.style.display = "none"
+            break;
+          }
+          switch (Numberss) {
+            case 1:
+                buttonsColoring2(yesNumbers, noNumbers)
+            break;
+            case 2:
+                buttonsColoring1(yesNumbers, noNumbers)
+            break;
+          }
+          switch (Specials) {
+            case 1:
+                buttonsColoring2(yesSpecials, noSpecials)
+            break;
+            case 2:
+                buttonsColoring1(yesSpecials, noSpecials)
+            break;
+          }
+          switch (Uppers) {
+            case 1:
+                buttonsColoring2(yesCaps, noCaps)
+            break;
+            case 2:
+                buttonsColoring1(yesCaps, noCaps)
+            break;
+          }
+    }
+    buttonsStyle()
+    generateBTN.addEventListener("click", function() {
+        charQuan = charValue.value
+        genQuan = genValue.value
+        joinType = joinPattern.value
+        errorEl.style.color = "hsl(0, 100%, 75%)"
+        setTimeout(() => {
+            errorEl.style.color = "hsl(0, 100%, 55%)"
+        }, 500);
+        if (charQuan === "" || genQuan === "") {
+            errorEl.textContent = "Empty input spaces found";
+        }else if(charQuan > 35 || charQuan < 1){
+            errorEl.textContent = "You code length cannot be greater than 35 or lower than 1 character";
+        } else if(Numberss === 2 && Characterss == 2 && Specials == 2) {
+            errorEl.textContent = "All of the 3 type characters are set as 'no' at least one character type required";
+        }
+        else {
+            errorEl.textContent = "";
+            charValue.value = ""
+            genValue.value = ""
+    
+            window.location = "./Editor/genPage"
+            function setItem(id, value) {
+                localStorage.setItem(id, value.toString());
+            }
+            
+            setItem("generates-quantity", charQuan);
+            setItem("Characters-quantity", genQuan);
+            setItem("characters", Characterss);
+            setItem("tempSel", tempSelected);
+            setItem("specials", Specials);
+            setItem("numbers", Numberss);
+            setItem("type", joinType);
+            setItem("upper", Uppers);
+
+        }
+        errorEl.style.transition = "color 0.5s ease"
+    });
+
+function idkNameYet() {
+        let randNum = Math.floor(Math.random() * 9) + 1
+        nrScUI.textContent = ArrayUI.join('') + randNum
+        ArrayUI.push(randNum)
+    }
+    setInterval(idkNameYet, 25)
+
+    function coloringBoiler(template){
+        hiddenEl2.style.backgroundColor = "hsla(240, 100%, 70%, 0.233)";
+        template.style.borderColor = "hsl(240, 100%, 70%)"
+        template.style.color = "hsl(240, 100%, 70%)"
+        localStorage.setItem("tempSel", tempSelected.toString());
+        hiddenEl2.style.display = "block"
+        hiddenEl2.style.transition = "backgroundColor 1s ease";
+    }
+    templateOne.addEventListener("click", function(){
+        if(tempSelected == 2){
+            templateTwo.style.borderColor = "black"
+            templateTwo.style.color = "black"
+            }
+            coloringBoiler(templateOne)
+            setTimeout(() => {
+                hiddenEl2.style.backgroundColor = "hsla(240, 100%, 70%, 0.192)";
+            }, 222);
+            tempSelected = 1
+        })
+    templateTwo.addEventListener("click", function(){
+        if(tempSelected == 1){
+            templateOne.style.borderColor = "black"
+            templateOne.style.color = "black"
+            }
+            coloringBoiler(templateTwo)
+            setTimeout(() => {
+                hiddenEl2.style.backgroundColor = "hsla(240, 100%, 70%, 0.192)";
+            }, 222);
+            tempSelected = 2
+        })
+    unSelect.addEventListener("click", function(){
+        templateOne.style.borderColor = "black"
+        templateOne.style.color = "black"
+        templateTwo.style.borderColor = "black"
+        templateTwo.style.color = "black"
+        tempSelected = 0
+        hiddenEl2.style.display = "none"
+    })
+
+})
